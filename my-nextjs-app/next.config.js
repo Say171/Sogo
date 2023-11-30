20bf97ba8cae9a6723bd5a14858c66fce044c73b
Say H <chse22036@g.nihon-u.ac.jp>
2023-11-30 22:22:48 +0900

    *****

--- my-nextjs-app/next.config.js
+++ my-nextjs-app/next.config.js
@@ -1,5 +1,5 @@
 // my-nextjs-app/next.config.js
 module.exports = {
-    basePath: '/my-nextjs-app', // デプロイ先のディレクトリ構造に合わせて変更
-    assetPrefix: '/my-nextjs-app/', // デプロイ先のディレクトリ構造に合わせて変更
+    basePath: '', // デプロイ先のディレクトリ構造に合わせて変更
+    assetPrefix: '', // デプロイ先のディレクトリ構造に合わせて変更
 };
